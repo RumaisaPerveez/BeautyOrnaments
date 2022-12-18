72656d9d245f4d0450e2b3913ee76d0811f30faf
RumaisaPerveez <119033374+RumaisaPerveez@users.noreply.github.com>
2022-12-18 11:48:40 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php
+
+include('header.php');
+include_once('conn.php');
+$con = new connec();
+
+
+?>
+
+<main class="main-content">
+    <!--== Start Page Header Area Wrapper ==-->
+    <section class="page-header-area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-7 col-lg-7 col-xl-5">
+                    <div class="page-header-content">
+                        <div class="title-img"><img src="assets/images/photos/about-title.webp" alt="Image" height="200px;"></div>
+                        <h3 class="page-header-content text-center">We, Are Beauty Provider</h3>
+                        <h5 class="page-header-sub-title" style="font-size:18px">Best cosmetics & Jewellery</h5>
+                        <p class="page-header-desc">We established feminine label known for versatile, graceful and classy jewellery & cosmetic with the ability to formulate creative vision keeping its products parallel to the casual feel.</p>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-5 col-xl-7">
+                    <div class="page-header-thumb">
+                        <img src="assets/images/photos/about.png" width="570" height="669" style="border-radius: 25px;" alt="Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--== End Page Header Area Wrapper ==-->
+
+    <br>
+
+</main>
+
+<?php
+
+include('footer.php');
+
+?>
